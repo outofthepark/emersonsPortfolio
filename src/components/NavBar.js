889cb14e354d3926a2b0488eca6affb669d39480
Allison Harden <allison@sgiam.com>
2021-12-23 12:45:07 -0700

    Removing unused variables because netlify is treating warnings as errors.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function NavBar() {
     return <Navbar bg="dark" variant="dark">
