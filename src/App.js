52fa1be158beca34a9928ac0afd36e7be0265bb1
Allison Harden <allison@sgiam.com>
2021-12-23 12:41:00 -0700

    Added react-bootstrap and navbar component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-
+import NavBar from './components/NavBar'
+//import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path='/' exact element={<Home/>} />
         <Route path='/about' element={<About/>} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
+
+function NavBar() {
+    return <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar>;
+}
+
+export default NavBar;
